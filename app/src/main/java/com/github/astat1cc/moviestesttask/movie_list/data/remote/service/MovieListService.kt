ff0e1f5db13421c09535c3a8d56ecd311ba85fc0
Dinar <dinar.taziev.dev@gmail.com>
2022-07-30 18:48:32 +0300

    Api key added to build.gradle debug

--- app/src/main/java/com/github/astat1cc/moviestesttask/movie_list/data/remote/service/MovieListService.kt
+++ app/src/main/java/com/github/astat1cc/moviestesttask/movie_list/data/remote/service/MovieListService.kt
@@ -1,5 +1,6 @@
 package com.github.astat1cc.moviestesttask.movie_list.data.remote.service
 
+import com.github.astat1cc.moviestesttask.BuildConfig
 import com.github.astat1cc.moviestesttask.movie_list.data.remote.entities.MoviesResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,6 +10,6 @@ interface MovieListService {
     @GET("reviews/all.json")
     suspend fun fetchMovieList(
         @Query(value = "offset") offset: Int,
-        @Query(value = "api-key") apiKey: String = "y8KdYZjP2POaReybSvb2CGu2vH2wqo9S"
+        @Query(value = "api-key") apiKey: String = BuildConfig.API_KEY
     ): MoviesResponse
 }
